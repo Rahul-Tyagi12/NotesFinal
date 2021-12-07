0fe10eeb4f812c563c475bc4eccb1fc96a8e755a
Rahul-Tyagi12 <coles37916@hocking.edu>
2021-12-07 20:53:45 +0530

    ertf

---
+++ app/src/main/java/com/example/notesapptutorial/splash.java
@@ -0,0 +1,27 @@
+package com.example.notesapptutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(splash.this, MainActivity.class));
+                }
+            }
+        };thread.start();
+    }
+}
